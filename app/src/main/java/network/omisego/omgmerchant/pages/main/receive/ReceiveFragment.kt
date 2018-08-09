e66599b0c501c5e7293351ab3cba52a219815377
euro <phuchit@omise.co>
2018-08-09 16:47:52 +0700

    :art: Implement UI for spinner

--- app/src/main/java/network/omisego/omgmerchant/pages/main/receive/ReceiveFragment.kt
+++ app/src/main/java/network/omisego/omgmerchant/pages/main/receive/ReceiveFragment.kt
@@ -7,11 +7,15 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.fragment_receive.*
 import network.omisego.omgmerchant.R
 import network.omisego.omgmerchant.databinding.FragmentReceiveBinding
 
 class ReceiveFragment : Fragment() {
     private lateinit var binding: FragmentReceiveBinding
+    private lateinit var viewModel: ReceiveViewModel
+    private val mockTokens = listOf("OMG", "BTC", "ETH")
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(
@@ -25,7 +29,16 @@ class ReceiveFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val viewModel = ViewModelProviders.of(this)[ReceiveViewModel::class.java]
+        viewModel = ViewModelProviders.of(this)[ReceiveViewModel::class.java]
+        setupDataBinding()
+        setupSpinner()
+    }
+
+    private fun setupSpinner() {
+        spinner.adapter = ArrayAdapter<String>(spinner.context, R.layout.spinner_dropdown_item, mockTokens)
+    }
+
+    fun setupDataBinding() {
         binding.liveCalc = viewModel.liveCalculator
         binding.handler = viewModel.handler
         binding.setLifecycleOwner(this)
