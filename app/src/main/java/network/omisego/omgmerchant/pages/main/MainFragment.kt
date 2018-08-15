087087a27fd8d653bfe66812e42696ed98f35f7a
euro <phuchit@omise.co>
2018-08-15 16:54:59 +0700

    :art: Add toolbar in main fragment

--- app/src/main/java/network/omisego/omgmerchant/pages/main/MainFragment.kt
+++ app/src/main/java/network/omisego/omgmerchant/pages/main/MainFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
 import android.support.v4.graphics.drawable.DrawableCompat
+import android.support.v4.view.ViewPager
+import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +14,7 @@ import kotlinx.android.synthetic.main.fragment_main.*
 import network.omisego.omgmerchant.R
 import network.omisego.omgmerchant.extensions.get
 import network.omisego.omgmerchant.extensions.getDrawableCompat
+import network.omisego.omgmerchant.extensions.logi
 import network.omisego.omgmerchant.storage.Storage
 
 class MainFragment : Fragment() {
@@ -45,16 +48,44 @@ class MainFragment : Fragment() {
     }
 
     private fun initView() {
+        setupToolbar()
         pagerAdapter = MainPagerAdapter(childFragmentManager)
         viewpager.adapter = pagerAdapter
         tabLayout.setupWithViewPager(viewpager)
-
         with(tabLayout) {
             getTabAt(0)?.icon = tabLayout.context.getDrawableCompat(R.drawable.ic_tab_wallet)
             getTabAt(1)?.icon = tabLayout.context.getDrawableCompat(R.drawable.ic_tab_topup)
             getTabAt(2)?.icon = tabLayout.context.getDrawableCompat(R.drawable.ic_tab_more)
         }
         tintTabLayoutIcon()
+        changeToolbarTitleWhenPageChanged()
+    }
+
+    private fun changeToolbarTitleWhenPageChanged() {
+        viewpager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+            }
+
+            override fun onPageSelected(position: Int) {
+                val toolbarTitle = when (position) {
+                    0 -> "Receive"
+                    1 -> "Topup"
+                    2 -> "More"
+                    else -> throw IllegalStateException("Unsupported operation")
+                }
+                toolbar.title = toolbarTitle
+                logi(position)
+            }
+        })
+    }
+
+    private fun setupToolbar() {
+        val hostActivity = activity as AppCompatActivity
+        hostActivity.setSupportActionBar(toolbar)
+        toolbar.title = "Receive"
     }
 
     private fun tintTabLayoutIcon() {
