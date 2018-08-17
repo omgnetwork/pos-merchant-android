449c4110c6ebe8369e86b59a31afd10a24b1c7be
euro <phuchit@omise.co>
2018-08-17 17:16:35 +0700

    :sparkles: Provide more convenient ways to create ViewModel

--- app/src/main/java/network/omisego/omgmerchant/extensions/ViewModelProviders.kt
+++ app/src/main/java/network/omisego/omgmerchant/extensions/ViewModelProviders.kt
@@ -1,12 +1,5 @@
 package network.omisego.omgmerchant.extensions
 
-import android.arch.lifecycle.AndroidViewModel
-import android.arch.lifecycle.ViewModel
-import android.arch.lifecycle.ViewModelProviders
-import android.support.v4.app.Fragment
-import network.omisego.AndroidViewModelFactory
-import network.omisego.ViewModelFactory
-
 /*
  * OmiseGO
  *
@@ -14,10 +7,34 @@ import network.omisego.ViewModelFactory
  * Copyright © 2017-2018 OmiseGO. All rights reserved.
  */
 
+import android.arch.lifecycle.AndroidViewModel
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProviders
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import network.omisego.AndroidViewModelFactory
+import network.omisego.ViewModelFactory
+
+inline fun <reified T : ViewModel> Fragment.provideActivityViewModel(): T {
+    return ViewModelProviders.of(activity!!, ViewModelFactory())[T::class.java]
+}
+
+inline fun <reified T : AndroidViewModel> Fragment.provideActivityAndroidViewModel(): T {
+    return ViewModelProviders.of(activity!!, AndroidViewModelFactory(this.activity!!.application))[T::class.java]
+}
+
 inline fun <reified T : ViewModel> Fragment.provideViewModel(): T {
     return ViewModelProviders.of(this, ViewModelFactory())[T::class.java]
 }
 
 inline fun <reified T : AndroidViewModel> Fragment.provideAndroidViewModel(): T {
     return ViewModelProviders.of(this, AndroidViewModelFactory(this.activity!!.application))[T::class.java]
+}
+
+inline fun <reified T : ViewModel> AppCompatActivity.provideViewModel(): T {
+    return ViewModelProviders.of(this, ViewModelFactory())[T::class.java]
+}
+
+inline fun <reified T : AndroidViewModel> AppCompatActivity.provideAndroidViewModel(): T {
+    return ViewModelProviders.of(this, AndroidViewModelFactory(application))[T::class.java]
 }
