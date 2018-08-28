15174f30c3e7e05ef065369802e8d73c9a68f2b2
euro <phuchit@omise.co>
2018-08-28 11:59:00 +0700

    :art: Refactor `Select account`

--- app/src/main/java/network/omisego/omgmerchant/pages/account/SelectAccountFragment.kt
+++ app/src/main/java/network/omisego/omgmerchant/pages/account/SelectAccountFragment.kt
@@ -2,9 +2,10 @@ package network.omisego.omgmerchant.pages.account
 
 import android.arch.lifecycle.Observer
 import android.databinding.DataBindingUtil
+import android.graphics.Rect
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
@@ -16,8 +17,10 @@ import co.omisego.omisego.model.pagination.PaginationList
 import kotlinx.android.synthetic.main.fragment_select.*
 import network.omisego.omgmerchant.R
 import network.omisego.omgmerchant.base.LoadingRecyclerAdapter
+import network.omisego.omgmerchant.custom.MarginDividerDecorator
 import network.omisego.omgmerchant.databinding.FragmentSelectBinding
 import network.omisego.omgmerchant.databinding.ViewholderAccountBinding
+import network.omisego.omgmerchant.extensions.dpToPx
 import network.omisego.omgmerchant.extensions.provideViewModel
 import network.omisego.omgmerchant.extensions.toast
 
@@ -25,6 +28,7 @@ class SelectAccountFragment : Fragment() {
     private lateinit var binding: FragmentSelectBinding
     private lateinit var viewModel: SelectAccountViewModel
     private lateinit var adapter: LoadingRecyclerAdapter<Account, ViewholderAccountBinding>
+    private lateinit var dividerDecorator: MarginDividerDecorator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,11 +47,21 @@ class SelectAccountFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupToolbar()
         adapter = LoadingRecyclerAdapter(R.layout.viewholder_account_loading, R.layout.viewholder_account, viewModel)
+        val margin = Rect(context?.dpToPx(96f)!!, 0, 0, 0)
+        dividerDecorator = MarginDividerDecorator(context!!, margin)
+
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(context)
-        recyclerView.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+        recyclerView.addItemDecoration(dividerDecorator)
         listen()
+    }
+
+    private fun setupToolbar() {
+        val hostActivity = activity as AppCompatActivity
+        hostActivity.setSupportActionBar(toolbar)
+        hostActivity.supportActionBar?.title = getString(R.string.select_account_title)
     }
 
     private fun listen() {
