65982c6b5b2c4406ebee0cba57aed2fc7e656856
euro <phuchit@omise.co>
2018-09-01 17:35:45 +0700

    :text: Renaming color resources

--- app/src/main/java/network/omisego/omgmerchant/pages/main/more/transaction/TransactionBindingAdapter.kt
+++ app/src/main/java/network/omisego/omgmerchant/pages/main/more/transaction/TransactionBindingAdapter.kt
@@ -27,7 +27,7 @@ object TransactionBindingAdapter {
                 tv.setTextColor(ContextCompat.getColor(tv.context, R.color.colorRed))
             }
             else -> {
-                tv.setTextColor(ContextCompat.getColor(tv.context, R.color.colorGrayText))
+                tv.setTextColor(ContextCompat.getColor(tv.context, R.color.colorGrayWeak))
             }
         }
     }
