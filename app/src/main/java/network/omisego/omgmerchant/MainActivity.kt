8f5013e43c6f8693fcc67404a8e55bc28e68349e
euro <phuchit@omise.co>
2018-12-03 21:20:18 +0700

    :hammer: Remove log

--- app/src/main/java/network/omisego/omgmerchant/MainActivity.kt
+++ app/src/main/java/network/omisego/omgmerchant/MainActivity.kt
@@ -4,17 +4,12 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
-import network.omisego.omgmerchant.extensions.logi
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.AppTheme)
         setContentView(R.layout.activity_main)
-
-        findNavController(R.id.nav_host).addOnNavigatedListener { controller, _ ->
-            logi("NavGraph: current destination: ${controller.currentDestination?.label}")
-        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
